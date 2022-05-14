a28929a71939368a2a5c75a85c7b322a98fdeaaa
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-13 20:39:01 -0400

    Finished the functionality in terms of displaying the selected button value and the modal on the screen, currently working on the styling

--- src/script.js
+++ src/script.js
@@ -1,10 +1,29 @@
-let inputfield = document.querySelectorAll(".input-group");
+let buttons = document.querySelectorAll(".btn");
+let modal = document.querySelector(".modal");
+let openModal = document.querySelector(".submit-btn");
+let closeModal = document.querySelector(".close-btn");
+
+// Function responsible for displaying the text on the screen
+
 
 function processSelection() {
-  inputfield.forEach((input) => {
-    input.addEventListener("click", () => {
-      input.classList.add("active-rating");
-      console.log("Active rating enabled");
-    });
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      btn.classList.add('active-rating')
+      document.getElementById("selected-rating").innerHTML = btn.value;
+    })
   });
 }
+
+processSelection();
+
+
+// Function responsible for opening and closing the modal
+
+openModal.addEventListener("click", () => {
+  modal.show();
+})
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+})
